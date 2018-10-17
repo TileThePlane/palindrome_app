d0751a91ac4b92764f0b819fef2343eea7f520ff
Kyle <kvonderw@uci.edu>
2018-10-17 09:54:32 -0700

    edit layout

--- app.rb
+++ app.rb
@@ -1,17 +1,16 @@
 require 'sinatra'
 
 get '/' do
-	@title = 'Home'
-	erb :index, :layout => :page
+  @title = 'Home'
+  erb :index
 end
 
 get '/about' do
-	@title = 'About'
-	erb :about, :layout => :page
+  @title = 'About'
+  erb :about
 end
 
 get '/palindrome' do
-	@title = 'Palindrome
-	erb :palindrome, :layout => :page
+  @title = 'Palindrome Detector'
+  erb :palindrome
 end
-
