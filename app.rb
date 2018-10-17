504d231df10e5d9975a466f7011fd1da0259b253
Kyle <kvonderw@uci.edu>
2018-10-17 09:23:15 -0700

    Add dynamic title and tests

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require 'sinatra'
 
 get '/' do
+	@title = 'Home'
 	erb :index, :layout => :page
 end
 
 get '/about' do
+	@title = 'About'
 	erb :about, :layout => :page
 end
 
 get '/palindrome' do
+	@title = 'Palindrome
 	erb :palindrome, :layout => :page
 end
 
--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -11,17 +11,24 @@ class PalindromeAppTest < Minitest::Test
 		get '/'
 		assert last_response.ok?
 		assert doc(last_response).at_css('h1')
-	
+		title_content = doc(last_response).at_css('title').content
+		assert_equal "First Ruby App | Home", title_content
 	def test_about
 		get '/about'
 		assert last_response.ok?
 		assert doc(last_response).at_css('h1')
+		title_content = doc(last_response).at_css('title').content
+		assert_equal "First Ruby App | About", title_content
+
 	end
 
 	def test_palindrome
 		get '/palindrome'
 		assert last_response.ok?
 		assert doc(last_response).at_css('h1')
+		title_content = doc(last_response).at_css('title').content
+		assert_equal "First Ruby App | Palindrome Detector", title_content
+
 	end
 end
 
